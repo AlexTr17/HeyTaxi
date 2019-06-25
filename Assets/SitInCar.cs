cc0ea2408cebbab3b0711913f9e921d3778ff9bb
AlexTr17 <sachaknlu@gmail.com>
2019-06-25 23:43:33 +0300

    12

---
+++ Assets/SitInCar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SitInCar : MonoBehaviour
+{
+    private Animator animator;
+    private bool incar;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        incar = false;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Sut");
+            StartCoroutine("Sit");
+            gameObject.transform.SetParent(other.transform);
+        }
+    }
+    IEnumerator Sit()
+    {
+        animator.SetBool("SitInCar", true);
+        yield return new WaitForSeconds(0.5f);
+        animator.SetBool("SitInCar", false);
+        incar = true;
+        
+    }
+}
